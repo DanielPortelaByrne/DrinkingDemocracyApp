5a0db7b0f41267d7d02efe0319fcb19a78805778
Daniel Portela Byrne <danielportelabyrne@gmail.com>
2024-05-04 13:31:44 +0100

    Moved storePrompts function to external file

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -10,29 +10,21 @@ import {
   TextStyle,
   ViewStyle,
 } from "react-native";
-import {
-  screen1Styles,
-  nameInputComponentStyles,
-} from "../assets/styles/styles";
+import { screen1Styles } from "../assets/styles/styles";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Ionicons } from "@expo/vector-icons";
-import { prompts } from "../prompts";
-import { crazy } from "../crazy";
-import { flirty } from "../flirty";
-import { virus } from "../virus";
-import { virusend } from "../virusend";
 
 import { Text, View } from "../components/Themed";
 import { RootTabScreenProps } from "../types";
-import { updateNames, getNames } from "../components/nameStore";
+import { getNames } from "../components/nameStore";
 import { useFonts } from "expo-font";
-import { useFocusEffect } from "@react-navigation/native";
 import en from "../languages/en.json";
 import ga from "../languages/ga.json";
 import pl from "../languages/pl.json";
 import es from "../languages/es.json";
 import { LanguageData } from "../utils/language/LanguageData";
 import { NameInput } from "../components/NameInput";
+import { storePrompts } from "../utils/storePrompts";
 var language = "English";
 
 const languages: { [key: string]: Partial<LanguageData> } = {
@@ -40,92 +32,6 @@ const languages: { [key: string]: Partial<LanguageData> } = {
   Irish: ga,
   Polish: pl,
   Spanish: es,
-};
-
-// Store the prompts in async storage
-const storePrompts = async (language: string) => {
-  try {
-    // Convert the prompts array to a JSON string
-    // Save the prompts strings in async storage
-    try {
-      const response = await fetch(
-        "https://raw.githubusercontent.com/DanielPortelaByrne/DrinkingDemocracyApp/json-data/JSON/" +
-          language +
-          "/prompts.json"
-      );
-      const data = await response.json();
-      const promptsString = JSON.stringify(data);
-      await AsyncStorage.setItem("promptsPack", promptsString);
-      console.log("Successfully fetched and stored data: " + promptsString);
-    } catch (error) {
-      console.error(error);
-      console.log("prinks");
-      const promptsString = JSON.stringify(prompts);
-      await AsyncStorage.setItem("promptsPack", promptsString);
-    }
-    try {
-      const response = await fetch(
-        "https://raw.githubusercontent.com/DanielPortelaByrne/DrinkingDemocracyApp/json-data/JSON/" +
-          language +
-          "/crazy.json"
-      );
-      const data = await response.json();
-      const crazyString = JSON.stringify(data);
-      await AsyncStorage.setItem("crazyPack", crazyString);
-      console.log("Successfully fetched and stored data: " + crazyString);
-    } catch (error) {
-      console.error(error);
-      const crazyString = JSON.stringify(crazy);
-      await AsyncStorage.setItem("crazyPack", crazyString);
-    }
-    try {
-      const response = await fetch(
-        "https://raw.githubusercontent.com/DanielPortelaByrne/DrinkingDemocracyApp/json-data/JSON/" +
-          language +
-          "/flirty.json"
-      );
-      const data = await response.json();
-      const flirtyString = JSON.stringify(data);
-      await AsyncStorage.setItem("flirtyPack", flirtyString);
-      console.log("Successfully fetched and stored data: " + flirtyString);
-    } catch (error) {
-      console.error(error);
-      const flirtyString = JSON.stringify(flirty);
-      await AsyncStorage.setItem("flirtyPack", flirtyString);
-    }
-    try {
-      const response = await fetch(
-        "https://raw.githubusercontent.com/DanielPortelaByrne/DrinkingDemocracyApp/json-data/JSON/" +
-          language +
-          "/virus.json"
-      );
-      const data = await response.json();
-      const virusString = JSON.stringify(data);
-      await AsyncStorage.setItem("virusPack", virusString);
-      console.log("Successfully fetched and stored data: " + virusString);
-    } catch (error) {
-      console.error(error);
-      const virusString = JSON.stringify(virus);
-      await AsyncStorage.setItem("virusPack", virusString);
-    }
-    try {
-      const response = await fetch(
-        "https://raw.githubusercontent.com/DanielPortelaByrne/DrinkingDemocracyApp/json-data/JSON/" +
-          language +
-          "/virusend.json"
-      );
-      const data = await response.json();
-      const virusEndString = JSON.stringify(data);
-      await AsyncStorage.setItem("virusendPack", virusEndString);
-      console.log("Successfully fetched and stored data: " + virusEndString);
-    } catch (error) {
-      console.error(error);
-      const virusEndString = JSON.stringify(virusend);
-      await AsyncStorage.setItem("virusendPack", virusEndString);
-    }
-  } catch (error) {
-    console.error(error);
-  }
 };
 
 export default function TabOneScreen({
---
+++ utils/storePrompts.tsx
@@ -0,0 +1,92 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { prompts } from "../prompts";
+import { flirty } from "../flirty";
+import { crazy } from "../crazy";
+import { virus } from "../virus";
+import { virusend } from "../virusend";
+
+// Store the prompts in async storage
+export const storePrompts = async (language: string) => {
+  try {
+    // Convert the prompts array to a JSON string
+    // Save the prompts strings in async storage
+    try {
+      const response = await fetch(
+        "https://raw.githubusercontent.com/DanielPortelaByrne/DrinkingDemocracyApp/json-data/JSON/" +
+          language +
+          "/prompts.json"
+      );
+      const data = await response.json();
+      const promptsString = JSON.stringify(data);
+      await AsyncStorage.setItem("promptsPack", promptsString);
+      console.log("Successfully fetched and stored data: " + promptsString);
+    } catch (error) {
+      console.error(error);
+      console.log("prinks");
+      const promptsString = JSON.stringify(prompts);
+      await AsyncStorage.setItem("promptsPack", promptsString);
+    }
+    try {
+      const response = await fetch(
+        "https://raw.githubusercontent.com/DanielPortelaByrne/DrinkingDemocracyApp/json-data/JSON/" +
+          language +
+          "/crazy.json"
+      );
+      const data = await response.json();
+      const crazyString = JSON.stringify(data);
+      await AsyncStorage.setItem("crazyPack", crazyString);
+      console.log("Successfully fetched and stored data: " + crazyString);
+    } catch (error) {
+      console.error(error);
+      const crazyString = JSON.stringify(crazy);
+      await AsyncStorage.setItem("crazyPack", crazyString);
+    }
+    try {
+      const response = await fetch(
+        "https://raw.githubusercontent.com/DanielPortelaByrne/DrinkingDemocracyApp/json-data/JSON/" +
+          language +
+          "/flirty.json"
+      );
+      const data = await response.json();
+      const flirtyString = JSON.stringify(data);
+      await AsyncStorage.setItem("flirtyPack", flirtyString);
+      console.log("Successfully fetched and stored data: " + flirtyString);
+    } catch (error) {
+      console.error(error);
+      const flirtyString = JSON.stringify(flirty);
+      await AsyncStorage.setItem("flirtyPack", flirtyString);
+    }
+    try {
+      const response = await fetch(
+        "https://raw.githubusercontent.com/DanielPortelaByrne/DrinkingDemocracyApp/json-data/JSON/" +
+          language +
+          "/virus.json"
+      );
+      const data = await response.json();
+      const virusString = JSON.stringify(data);
+      await AsyncStorage.setItem("virusPack", virusString);
+      console.log("Successfully fetched and stored data: " + virusString);
+    } catch (error) {
+      console.error(error);
+      const virusString = JSON.stringify(virus);
+      await AsyncStorage.setItem("virusPack", virusString);
+    }
+    try {
+      const response = await fetch(
+        "https://raw.githubusercontent.com/DanielPortelaByrne/DrinkingDemocracyApp/json-data/JSON/" +
+          language +
+          "/virusend.json"
+      );
+      const data = await response.json();
+      const virusEndString = JSON.stringify(data);
+      await AsyncStorage.setItem("virusendPack", virusEndString);
+      console.log("Successfully fetched and stored data: " + virusEndString);
+    } catch (error) {
+      console.error(error);
+      const virusEndString = JSON.stringify(virusend);
+      await AsyncStorage.setItem("virusendPack", virusEndString);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
